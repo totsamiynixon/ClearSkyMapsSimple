cc79819f2731741eef5e96702c5cb1beadccbe53
totsamiynixon <totsamiynixon@gmail.com>
2020-09-16 18:57:41 +0300

    webconfig-transform.ps1 fix

--- deploy/basic/webconfig-transform.ps1
+++ deploy/basic/webconfig-transform.ps1
@@ -4,7 +4,7 @@ try
     [xml]$webConfig = Get-Content $path
     $envVarsEl = $webConfig.CreateElement('environmentVariables');
     $envVarEl = $webConfig.CreateElement('environmentVariable');
-    $envVarEl.SetAttribute("name", "ASPNETCORE_ENVIROMNENT");
+    $envVarEl.SetAttribute("name", "ASPNETCORE_ENVIRONMENT");
     $envVarEl.SetAttribute("value", $env:ASPNETCORE_ENVIRONMENT);
     $envVarsEl.AppendChild($envVarEl);
     $webConfig.configuration.location["system.webServer"].aspNetCore.appendChild($envVarsEl);
